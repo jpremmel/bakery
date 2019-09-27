8cbd63ef2b3db6f861ed82f81e462116b4db3815
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 13:42:34 -0700

    Trying to switch from try-catch to regex to test user input.

--- program.cs
+++ program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using Bakery;
 
 class Program
@@ -10,32 +11,39 @@ class Program
         Console.WriteLine("~ One pastry: $2. *Pierre's Deal: Buy three pastries for $5!* ~");
         Console.WriteLine("How many loaves of bread would you like to purchase?");
         int loaves = 0;
-        try
+        Regex regex = new Regex(@"^[0-9]+$");
+        string loavesInput = Console.ReadLine();
+        Match match = regex.Match(loavesInput);
+        if (match.Success)
         {
-            loaves = int.Parse(Console.ReadLine());
+
         }
-        catch (System.FormatException)
-        {
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("Numerical input only. Please try again.");
-            Console.WriteLine("---------------------------------------");
-            loaves = int.Parse(Console.ReadLine());
-        }
+        // try
+        // {
+        //     loaves = int.Parse(Console.ReadLine());
+        // }
+        // catch (System.FormatException)
+        // {
+        //     Console.WriteLine("---------------------------------------");
+        //     Console.WriteLine("Numerical input only. Please try again.");
+        //     Console.WriteLine("---------------------------------------");
+        //     loaves = int.Parse(Console.ReadLine());
+        // }
         Bread bread = new Bread();
         int breadPrice = bread.CalcPrice(loaves);
         Console.WriteLine("How many pastries would you like to purchase?");
         int pastries = 0;
-        try
-        {
-            pastries = int.Parse(Console.ReadLine());
-        }
-        catch (System.FormatException)
-        {
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("Numerical input only. Please try again.");
-            Console.WriteLine("---------------------------------------");
-            pastries = int.Parse(Console.ReadLine());
-        }
+        // try
+        // {
+        //     pastries = int.Parse(Console.ReadLine());
+        // }
+        // catch (System.FormatException)
+        // {
+        //     Console.WriteLine("---------------------------------------");
+        //     Console.WriteLine("Numerical input only. Please try again.");
+        //     Console.WriteLine("---------------------------------------");
+        //     pastries = int.Parse(Console.ReadLine());
+        // }
         Pastry pastry = new Pastry();
         int pastryPrice = pastry.CalcPrice(pastries);
         int totalPrice = breadPrice + pastryPrice;

a5401f82509eb2cfd55a774c1d4e219cb04e0c85
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 11:20:37 -0700

    Update pastry price calculation to include bargain.

--- bakery.cs
+++ bakery.cs
@@ -1,38 +1,28 @@
-using System;
-
 namespace Bakery
 {
     class Bread
     {
         public int LoafPrice { get; set; }
-
         public Bread()
         {
             LoafPrice = 5;
         }
-
         public int CalcPrice(int loaves)
         {
             int price = (loaves / 3) * (LoafPrice * 2) + (LoafPrice * (loaves % 3));
             return price;
         }
     }
-
     class Pastry
     {
         public int PastryPrice { get; set; }
-
         public Pastry()
         {
             PastryPrice = 2;
         }
-
         public int CalcPrice(int pastries)
         {
-            int price = 0;
-
-            price = PastryPrice * pastries;
-
+            int price = (pastries / 3) * 5 + (PastryPrice * (pastries % 3));
             return price;
         }
     }

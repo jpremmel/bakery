45cee6229e2689383f8435e58dd2df49e8de9848
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 08:57:54 -0700

    Add price properties and constructors for Bread and Pastry classes.

--- bakery.cs
+++ bakery.cs
@@ -4,11 +4,22 @@ namespace Bakery
 {
     class Bread
     {
+        public int LoafPrice { get; set; }
+
+        public Bread()
+        {
+            LoafPrice = 5;
+        }
 
     }
 
     class Pastry
     {
-        
+        public int PastryPrice { get; set; }
+
+        public Pastry()
+        {
+            PastryPrice = 2;
+        }
     }
 }

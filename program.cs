664ad0884fc7471d9a44e490bf5508861d180807
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 11:46:20 -0700

    Add try-catch blocks to encourage user to enter only numerical input.

--- program.cs
+++ program.cs
@@ -9,11 +9,33 @@ class Program
         Console.WriteLine("~ One loaf of bread: $5. *Pierre's Deal: Buy two, get one free!* ~");
         Console.WriteLine("~ One pastry: $2. *Pierre's Deal: Buy three pastries for $5!* ~");
         Console.WriteLine("How many loaves of bread would you like to purchase?");
-        int loaves = int.Parse(Console.ReadLine());
+        int loaves = 0;
+        try
+        {
+            loaves = int.Parse(Console.ReadLine());
+        }
+        catch (System.FormatException e)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Numerical input only. Please try again.");
+            Console.WriteLine("---------------------------------------");
+            loaves = int.Parse(Console.ReadLine());
+        }
         Bread bread = new Bread();
         int breadPrice = bread.CalcPrice(loaves);
         Console.WriteLine("How many pastries would you like to purchase?");
-        int pastries = int.Parse(Console.ReadLine());
+        int pastries = 0;
+        try
+        {
+            pastries = int.Parse(Console.ReadLine());
+        }
+        catch (System.FormatException e)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Numerical input only. Please try again.");
+            Console.WriteLine("---------------------------------------");
+            pastries = int.Parse(Console.ReadLine());
+        }
         Pastry pastry = new Pastry();
         int pastryPrice = pastry.CalcPrice(pastries);
         int totalPrice = breadPrice + pastryPrice;

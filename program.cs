4a6c7488038aa5316ecdcc46207de052b00bcb00
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 08:52:45 -0700

    Add prompts to see what user would like to purchase.

--- program.cs
+++ program.cs
@@ -6,5 +6,12 @@ class Program
     static void Main()
     {
         Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("One loaf of bread: ");
+        Console.WriteLine("One pastry: ");
+        Console.WriteLine("How many loaves of bread would you like to purchase?");
+        int loaves = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pastries would you like to purchase?");
+        int pastries = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your total price: ");
     }
 }

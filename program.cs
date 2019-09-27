9bc6ffb9a7936c0442fc2022634ee7309d104d5f
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-27 11:26:01 -0700

    Update spacing.

--- program.cs
+++ program.cs
@@ -6,19 +6,16 @@ class Program
     static void Main()
     {
         Console.WriteLine("~ Welcome to Pierre's Bakery! ~");
-        Console.WriteLine("~ One loaf of bread: $5. *Deal: Buy two, get one free!* ~");
-        Console.WriteLine("~ One pastry: $2. *Deal: Buy three pastries for $5!* ~");
-
+        Console.WriteLine("~ One loaf of bread: $5. *Pierre's Deal: Buy two, get one free!* ~");
+        Console.WriteLine("~ One pastry: $2. *Pierre's Deal: Buy three pastries for $5!* ~");
         Console.WriteLine("How many loaves of bread would you like to purchase?");
         int loaves = int.Parse(Console.ReadLine());
         Bread bread = new Bread();
         int breadPrice = bread.CalcPrice(loaves);
-
         Console.WriteLine("How many pastries would you like to purchase?");
         int pastries = int.Parse(Console.ReadLine());
         Pastry pastry = new Pastry();
         int pastryPrice = pastry.CalcPrice(pastries);
-        
         int totalPrice = breadPrice + pastryPrice;
         Console.WriteLine("Your total price: $" + totalPrice);
     }
